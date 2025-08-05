2cf5bb36b259d0878dbc4664e3c142371c32b0c9
AdamKhelladi <khelladiadem5@gmail.com>
2025-08-05 11:42:55 +0100

    Add the dataset and test it

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,14 +4,50 @@ import ToggleButtons from "./ToggleButtons";
 import Todo from "./Todo";
 import InputTodo from "./InputTodo";
 
+import { v4 as uuidv4 } from 'uuid';
+
+import { useState } from "react";
+
+const initialTodos = [
+  {
+    id: uuidv4(),
+    title: "Reading Books",
+    details: "The Book That Iam Reading Now Is Fosol Fi Osol Al Tafsir",
+    isComplited: false
+  },
+  {
+    id: uuidv4(),
+    title: "Go To The Gym",
+    details: "Go To The Gym Go To The Gym Go To The Gym",
+    isComplited: false
+  },
+  {
+    id: uuidv4(),
+    title: "Learn Programming",
+    details: "Do Some Code, & Tech & business",
+    isComplited: false
+  },
+  {
+    id: uuidv4(),
+    title: "Visit Family",
+    details: "My GrandPa & GrandMa, And Some Friends",
+    isComplited: false
+  }
+]
+
 export default function Content() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const todosJsx = todos.map((todo) => {
+    return <Todo key={todo.id} title={todo.title} details={todo.details}/>
+  });
+
   return (
     <Container>
       <div className="content">
         <ToggleButtons />
-        <Todo />
-        <Todo />
-        <Todo />
+
+        {todosJsx}
 
         <InputTodo />
       </div>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,7 +7,7 @@ const IconStyle = {
   "&:hover": { color: "#fff" },
 };
 
-export default function Todo() {
+export default function Todo({ title, details }) {
   return (
     <Card
       sx={{
@@ -24,11 +24,11 @@ export default function Todo() {
         <Grid container alignItems="center" justifyContent="space-between">
           <Grid item xs={9}>
             
-            <Typography variant="h5" sx={{ color: "#ccc", textAlign: "left"}}>
-              First TODO
+            <Typography variant="h5" sx={{ color: "#ccc", textAlign: "left", fontWeight: "600", fontSize: "20px"}}>
+              {title}
             </Typography>
-            <Typography variant="h6" sx={{ color: "#999", textAlign: "left"}}>
-              Details 
+            <Typography variant="h6" sx={{ color: "#999", textAlign: "left", fontSize: "18px"}}>
+              {details} 
             </Typography>
           </Grid>
           <Grid item xs={3} sx={{ textAlign: "right" }}>
